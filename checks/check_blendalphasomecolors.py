05a37260fefa22303b85ed4e6509354a6fcbcb08
aleju <kontakt@ajung.name>
2020-01-08 15:10:37 +0100

    Add image URLs to check script

--- checks/check_blendalphasomecolors.py
+++ checks/check_blendalphasomecolors.py
@@ -18,15 +18,27 @@ def main():
             iaa.AveragePooling(8)
     ]))
 
-    url = (
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/"
-        "Vincent_van_Gogh_-_Wheatfield_with_crows_-_Google_Art_Project.jpg/"
-        "320px-Vincent_van_Gogh_-_Wheatfield_with_crows_-_Google_Art_Project"
-        ".jpg")
-    img = imageio.imread(url)
+    urls = [
+        ("https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/"
+         "Sarcophilus_harrisii_taranna.jpg/"
+         "320px-Sarcophilus_harrisii_taranna.jpg"),
+        ("https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/"
+         "Vincent_van_Gogh_-_Wheatfield_with_crows_-_Google_Art_Project.jpg/"
+         "320px-Vincent_van_Gogh_-_Wheatfield_with_crows_-_Google_Art_Project"
+         ".jpg"),
+        ("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/"
+         "Galerella_sanguinea_Zoo_Praha_2011-2.jpg/207px-Galerella_sanguinea_"
+         "Zoo_Praha_2011-2.jpg"),
+        ("https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/"
+         "Ambrosius_Bosschaert_the_Elder_%28Dutch_-_Flower_Still_Life_-_"
+         "Google_Art_Project.jpg/307px-Ambrosius_Bosschaert_the_Elder_%28"
+         "Dutch_-_Flower_Still_Life_-_Google_Art_Project.jpg")
+    ]
 
-    ia.imshow(ia.draw_grid(aug(images=[img]*25), cols=5, rows=5))
-    ia.imshow(ia.draw_grid(aug2(images=[img]*25), cols=5, rows=5))
+    for url in urls:
+        img = imageio.imread(url)
+        ia.imshow(ia.draw_grid(aug(images=[img]*25), cols=5, rows=5))
+        ia.imshow(ia.draw_grid(aug2(images=[img]*25), cols=5, rows=5))
 
 if __name__ == "__main__":
     main()
